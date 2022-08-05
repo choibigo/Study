147a4ded7692f92c2a682ce99b464920f68d9966
최대원 <daewon96.choi@mirero.co.kr>
2022-08-05 15:56:37 +0900

    ef core

---
+++ c#/EF Cores/EFCore/EFCore/Models/Empolyee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EFCore.Models
+{
+	//[Table("EMP")] // C# 에서는 Empolyee라고 하고 SQL 에서는 EMP라 한다. 를 명시
+	public class Empolyee
+	{
+		//[Key] // ID 가 Primary Key임을 명시 
+		public int Id { get; set; } // id 이름으로 만들면 자동으로 Primary Key가 된다.
+		public string Name { get; set; }
+		public decimal salary { get; set; }
+	}
+	// 테이블을 구조(메타데이터)를 정의 한다.
+	// 컬럼을 정의하고 컬럼명을 정의 한다.
+}
---
+++ c#/EF Cores/EFCore/EFCore/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using EFCore.Models;
+
+namespace EFCore
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			// 연결할 DB정의
+			MyDbContext db = new MyDbContext(); // 연결할 DB의 객체를 만든다.
+
+			// insert
+			Empolyee emp = new Empolyee
+			{
+				Name = "Daewon",
+				salary = 1000
+			};
+
+			db.Employees.Add(emp);
+			// db 객체에 Employee에 emp를 메모리상 추가 한다.
+
+
+			Empolyee emp2 = new Empolyee
+			{
+				Name = "Lil Daewon",
+				salary = 100000
+			};
+
+			db.Employees.Add(emp2);
+
+			db.SaveChanges(); // db값을 실제 데이터 베이스에 저장한다.
+
+			// select
+			var emps = db.Employees.Where(p => p.Id >= 1);
+			//연결할DB.찾을 Table.Where절 사용 coloum의 id가 1보다 큰경우 
+
+			foreach (var item in emps)
+			{
+				Console.WriteLine($"{item.Id} , {item.Name}");
+			}
+		}
+	}
+}
