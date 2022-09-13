fd8a4d5a26d4b46e30fc5597244da0d9e116e706
최대원 <daewon96.choi@mirero.co.kr>
2022-09-13 15:37:48 +0900

    classification - Training - Inference

--- Deep Learning 스터디/Pytorch/classificatin/classification_inference.py
+++ Deep Learning 스터디/Pytorch/classificatin/classification_inference.py
@@ -1,79 +1,62 @@
 import json
 import os
 import pickle
-from PIL import Image
-import numpy as np
 import torch
-import torch.nn as nn
 import torchvision.transforms as transforms
+import grpc
+import mpp
+from google.protobuf import wrappers_pb2
 
 class ModelHandler:
-    def __init__(self, data, context):
-        self._initialize()
-        model_config_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "model.json")
-        with open(model_config_file_path, "r", encoding='utf-8') as config_file:
-            self.config = json.load(config_file)
-            self.model_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), self.config['model_file'])
+    def __init__(self, model_path, resize = 32):
+        self.__resize = resize
+        self.__device = "cpu"
 
-        if self._framework_type.lower() == 'pytorch':
-            self.model = self._load_model()
+        self.__model_file_path = model_path
+        self.__model = self.__load_model()
 
-        else:
-            raise NotImplementedError
-
-    def __call__(self, data, context):
-        if self._framework_type.lower() == 'pytorch':
-            return self.inference(data, context)
-        else:
-            raise NotImplementedError
-
-    def _initialize(self):
-        self._framework_type = "pytorch"
-        self.device = 'cpu'
-
-    def _load_model(self):
-        model = torch.jit.load(self.model_file_path)
-        model.eval().to(self.device)
-        return model
-
-    def inference(self, data, context, *args, **kwargs):
-        data = pickle.loads(data)
-        data = self._preprocessing(data)
-        output = self.model(data)
-        output = self._postprocessing(output)
-        return output, context
-
-    def _preprocessing(self, image):
-        image = Image.fromarray(np.uint8(image))
-        transform = transforms.Compose([
-                    transforms.ToTensor()])
-        image = transform(image)
-        image = image.unsqueeze(0)
-
-        return image
-
-    def _postprocessing(self, result):
-        output = dict()
-        for i, score in enumerate(result[0][0], 1):
-            output[i] = score.item()
-
+    def __call__(self, data):
+        output = self.__inference(data)
         return output
 
+    def __load_model(self):
+        model = torch.jit.load(self.__model_file_path, map_location=self.__device)
+        model.eval()
+        return model
+
+    def __inference(self, data):
+        data = self.__preprocessing(data)
+        output = self.__model(data)
+        output = self.__postprocessing(output)
+        return output
+
+    def __preprocessing(self, image):
+        transform = transforms.Compose([
+                    transforms.ToTensor(),
+                    transforms.Resize((self.__resize,self.__resize))])
+        image = transform(image)
+        image = image.unsqueeze(0)
+        return image
+
+    def __postprocessing(self, result):
+        output = [score.item() for score in result[0][0]]
+        return output
 
 if __name__ == "__main__":
     
     import cv2
-    from skimage.io import imsave
-
-    test_data_path = r"D:\temp\inference\data\DNCNN\02_dncnn_input.png"
+    test_data_path = r"D:\workspace\data\Classification\CIFAR-10-images-master\train\ship\0000.jpg"
     test_data = cv2.imread(test_data_path, 1)
-    test_data_pickle = pickle.dumps(test_data)
 
-    # =================================================================== #
+    model_path = r"D:\temp\save_model\GoogleNet\googlenet_45.pth"
 
-    handler = ModelHandler(None, None)
-    inference_result, context =  handler(test_data_pickle, None)
+    # # =================================================================== #
 
-    # =================================================================== #
+    handler = ModelHandler(model_path)
+    inference_result =  handler(test_data)
+
+    # # =================================================================== #
 
     print(inference_result)
+    print(inference_result.index(max(inference_result)))
+   
