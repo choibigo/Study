1fe9f36e61659fbcdda3dbd818bf342e58d25bed
최대원 <daewon96.choi@mirero.co.kr>
2022-10-04 15:43:27 +0900

    fast-scnn train, eval 수정

--- Deep Learning 스터디/논문읽기/논문 구현/Fast-SCNN/eval.py
+++ Deep Learning 스터디/논문읽기/논문 구현/Fast-SCNN/eval.py
@@ -3,8 +3,10 @@ import numpy as np
 import torch
 from torchvision import transforms
 
+epoch = 13
+
 def eval(image_path):
-    model = torch.jit.load(r"D:\Model_Inference\save_model\Fast-Scnn\48.pth", map_location='cpu').eval()
+    model = torch.jit.load(f"D:\\Model_Inference\\save_model\\Fast-Scnn\\{epoch}_weight_none.pth", map_location='cpu').eval()
 
     transfom= transforms.Compose([
         transforms.ToTensor()
@@ -21,14 +23,16 @@ def eval(image_path):
     return predict.astype(np.uint8)
 
 if __name__ == '__main__':
-    result = eval(r"D:\workspace\data\ADI_BALL_768\image\1.png")
+    result = eval(r"D:\workspace\data\ADI_BALL\image\2.png")
 
     from PIL import Image
     palette = [
-        0, 0, 0,
+        44, 44, 44,
         255, 255, 255
     ]
     mask = Image.fromarray(result.astype(np.uint8))
     mask.putpalette(palette)
-    save_path= "D:\\Model_Inference\\inference\\Fast-SCNN\\fast_scnn_result.png"
+    save_path= f"D:\\Model_Inference\\inference\\Fast-SCNN\\fast_scnn_result_{epoch}_weight_none.png"
     mask.save(save_path)
+
+    print("Success")
