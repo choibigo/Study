44a4eca968bba9f09a5d86afeb40d2a6878aad55
최대원 <daewon96.choi@mirero.co.kr>
2022-10-11 16:46:39 +0900

    deeplab-v3

---
+++ Deep Learning 스터디/논문읽기/논문 구현/DeepLabV3/eval.py
@@ -0,0 +1,38 @@
+import cv2
+import numpy as np
+import torch
+from torchvision import transforms
+
+iter = 4100
+
+def eval(image_path):
+    model = torch.jit.load(f"D:\\Model_Inference\\save_model\\DeeplabV3\\{iter}.pth", map_location='cpu').eval()
+
+    transfom= transforms.Compose([
+        transforms.ToTensor()
+    ])
+
+    image = cv2.imread(image_path, 1)
+    image = transfom(image)
+    image = torch.unsqueeze(image, dim=0)
+    outputs = model(image)
+
+    pred = torch.argmax(outputs, 1)
+    pred = pred.cpu().data.numpy()
+    predict = pred.squeeze(0)
+    return predict.astype(np.uint8)
+
+if __name__ == '__main__':
+    result = eval(r"D:\workspace\data\ADI_BALL\image\2.png")
+
+    from PIL import Image
+    palette = [
+        44, 44, 44,
+        255, 255, 255
+    ]
+    mask = Image.fromarray(result.astype(np.uint8))
+    mask.putpalette(palette)
+    save_path= f"D:\\Model_Inference\\inference\\DeeplabV3\\deeblabv3_{iter}.png"
+    mask.save(save_path)
+
+    print("Success")
