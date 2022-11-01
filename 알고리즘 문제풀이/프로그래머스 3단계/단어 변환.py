5670a46cd4de341e77738403930d7213e130d4dc
최대원 <daewon96.choi@mirero.co.kr>
2022-11-02 08:43:53 +0900

    프로그래머스 3단계 1문제

--- 알고리즘 문제풀이/프로그래머스 3단계/단어 변환.py
+++ 알고리즘 문제풀이/프로그래머스 3단계/단어 변환.py
@@ -1,117 +1,42 @@
-# region BFS
 from collections import deque
 
-def generate_graph(begin, target, words):
-    
-    if begin not in words:
-        words.append(begin)
-    if target not in words:
-        words.append(target)
-    
-    graph = dict()
-    for word in words:
-        graph[word] = list()
-    
-    for i in range(len(words)):
-        for j in range(i+1, len(words)):
-            
-            count = len(words[i])
-            for w1, w2 in zip(words[i], words[j]):
-                if w1 == w2:
-                    count -= 1
-            
-            if count <=1:
-                graph[words[i]].append(words[j])
-                graph[words[j]].append(words[i])
-    
-    return graph
-        
+def check(word1, word2):
+    count = 0
+    for w1, w2 in zip(word1, word2):
+        if w1 != w2:
+            count +=1
+        if count >1 : 
+            return False
+    return True
             
 def solution(begin, target, words):
 
-    if target not in words: return 0
+    if target not in words:
+        return 0
     
-    graph = generate_graph(begin, target, words)
-    
-    visited = dict()
-    for word in words:
-        visited[word] = False
+    graph = {key:[] for key in words}
+    visited = {key:False for key in words}
+    for i in range(len(words)):
+        for j in range(i+1, len(words)):
+            if check(words[i], words[j]):
+                graph[words[i]].append(words[j])
+                graph[words[j]].append(words[i])
     
     nodes = deque()
-    nodes.append([begin, 0])
+    for key in words:
+        if check(begin, key):
+            nodes.append([key, 1])
+            visited[key] = True
     
     while nodes:
-        pop_word, pop_count = nodes.popleft()
+        pop_word, count = nodes.popleft()
         
         if pop_word == target:
-            return pop_count
+            return count
         
-        for g in graph[pop_word]:
-            if visited[g] == False:
-                visited[g] = True
-                nodes.append([g, pop_count+1])
-    
+        for next_word in graph[pop_word]:
+            if not visited[next_word]:
+                visited[next_word] = True
+                nodes.append([next_word, count+1])
+                
     return 0
-# endregion
-
-# region DFS
-def generate_graph(begin, target, words):
-    
-    if begin not in words:
-        words.append(begin)
-    if target not in words:
-        words.append(target)
-    
-    graph = dict()
-    for word in words:
-        graph[word] = list()
-    
-    for i in range(len(words)):
-        for j in range(i+1, len(words)):
-            
-            count = len(words[i])
-            for w1, w2 in zip(words[i], words[j]):
-                if w1 == w2:
-                    count -= 1
-            
-            if count <=1:
-                graph[words[i]].append(words[j])
-                graph[words[j]].append(words[i])
-    
-    return graph
-
-global min_count
-
-def DFS(v, word, target, graph, visited):
-    global min_count
-    
-    if word == target:
-        if min_count > v:
-            min_count = v
-            return 
-    
-    for g in graph[word]:
-        if visited[g] == False:
-            visited[g] = True
-            DFS(v+1, g, target, graph, visited)
-            visited[g] = False
-            
-def solution(begin, target, words):
-
-    if target not in words: return 0
-    
-    graph = generate_graph(begin, target, words)
-    
-    visited = dict()
-    for word in words:
-        visited[word] = False
-    
-    global min_count
-    min_count = len(words)+1
-    
-    visited[begin] = True
-    
-    DFS(0, begin, target, graph, visited)
-    
-    return min_count
-# endregion
