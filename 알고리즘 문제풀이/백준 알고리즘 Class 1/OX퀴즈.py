07346eef78283e5b8fd5968fa42ef5b47c22329e
최대원 <>
2022-11-25 16:16:42 +0900

    프로그래머스 1단계 7문제

--- 알고리즘 문제풀이/백준 알고리즘 Class 1/OX퀴즈.py
+++ 알고리즘 문제풀이/백준 알고리즘 Class 1/OX퀴즈.py
@@ -1,12 +1,15 @@
 import input_setting
 
+n = int(input())
+for _ in range(n):
+    result = input()
+    res = [0 for _ in range(len(result))]
+    res[0] = 1 if result[0] == "O" else 0
 
-for _ in range(int(input())):
-    string = input()
-    res = [0 for _ in range(len(string))]
-    res[0] = 1 if string[0] == 'O' else 1
-    for i in range(1, len(string)):
-        if string[i] == 'O':
-            res[i] = res[i-1]+1
+    for i in range(1, len(result)):
+        if result[i] == "O":
+            res[i] = res[i-1] + 1
 
     print(sum(res))
+
+print(ord('A'))
